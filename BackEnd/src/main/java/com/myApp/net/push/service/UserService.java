955adc9629f8f0bba45066778a6d35dcf235e9ab
FeifanCheng <85103419+FeifanCheng@users.noreply.github.com>
2021-10-09 05:42:25 +1100

    missing two import

--- BackEnd/src/main/java/com/myApp/net/push/service/UserService.java
+++ BackEnd/src/main/java/com/myApp/net/push/service/UserService.java
@@ -12,6 +12,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
+import java.util.List;
+
+import java.util.stream.Collectors;
 
 // http://localhost:8080/api/user
 @Path("/user")
